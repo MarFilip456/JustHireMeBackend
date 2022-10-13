5c70dd7b8759cb61faa33abad506eea2844958e4
MarFilip456 <marcin.filipkowski64@gmail.com>
2022-10-13 11:33:24 +0200

    another try

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { OfferModule } from './offer/offer.module';
 import { AuthModule } from './auth/auth.module';
 /* 'mongodb://localhost/nest' */
+/* process.env.MONGODB_URI */
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
     OfferModule,
     AuthModule,
   ],
